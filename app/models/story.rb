abc1432e31e242325ffb8ee8c3e13060fb1bf20a
edenawoke9 <135725390+edenawoke9@users.noreply.github.com>
2025-02-19 16:55:30 +0300

    updated validations

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,5 +1,7 @@
 class Story < ApplicationRecord
   belongs_to :user
-  validate :image, presence: true 
+  validates :image, presence: true, content_type: ['image/png', 'image/jpg', 'image/jpeg']
+
+
   validates :body, length: { maximum: 500 }
 end
